bc4f5fcec44f722a0e833b20bd5efe5cc2d48c7d
David Arnold <david.arnold@iohk.io>
2022-02-23 17:43:14 -0500

    imp(cardano): do restore from public snapshots
    - ACHTUNG: if lstate & sql-state grow out of sync, `db-sync` is doomed

--- cells/cardano/entrypoints/db-sync-entrypoint.sh
+++ cells/cardano/entrypoints/db-sync-entrypoint.sh
@@ -13,8 +13,33 @@ export PGPASSFILE="${PGPASSFILE}.permissioned"
 [ -z "${socketPath:-}" ] && echo "socketPath env var must be set -- aborting" && exit 1
 [ -z "${stateDir:-}" ] && echo "stateDir env var must be set -- aborting" && exit 1
 [ -z "${envFlag:-}" ] && echo "envFlag env var must be set -- aborting" && exit 1
+[ -z "${snapShotUrl:-}" ] && echo "snapShotUrl env var must be set -- aborting" && exit 1
+[ -z "${snapShotSha:-}" ] && echo "snapShotSha env var must be set -- aborting" && exit 1
 
 mapfile -t envFlag <<< "${envFlag}"
+
+function restore_snapshot {
+	file_count=$(find "${stateDir}" -type f -name '*.lstate' | wc -l)
+	if test "${file_count}" -gt 0 ; then
+	  echo "Ledger state directory (${stateDir}) is not empty. Don't restore from snapshot."
+	  return
+	fi
+
+  curl -LOC - "${snapShotUrl}"
+  curl -LO "${snapShotUrl}.sha256sum"
+  sha256sum -c "$(basename "${snapShotUrl}").sha256sum"
+
+	tmp_dir=$(mktemp --directory -t db-sync-snapshot-XXXXXXXXXX)
+	tar -zxvf "$(basename "${snapShotUrl}")" --directory "$tmp_dir"
+	db_file=$(find "$tmp_dir/" -iname "*.sql")
+	lstate_file=$(find "${tmp_dir}/" -iname "*.lstate")
+	mv "${lstate_file}" "${stateDir}"
+	psql --dbname="$(cut -d ":" -f 3 "${PGPASSFILE}")" -f "${db_file}"
+	rm --recursive "${tmp_dir}"
+}
+
+restore_snapshot
+
 
 until [ -S "${socketPath}" ]; do
   echo "Waiting 10 seconds for cardano-node socket file at ${socketPath}..."
--- cells/cardano/entrypoints/default.nix
+++ cells/cardano/entrypoints/default.nix
@@ -34,6 +34,16 @@ in
         else if envName == "mainnet"
         then "--mainnet"
         else abort "unreachable";
+      snapShotUrl = {
+        # https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/index.html#12/
+        "testnet" = "https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/12/db-sync-snapshot-schema-12-block-3336499-x86_64.tgz";
+        # https://update-cardano-mainnet.iohk.io/cardano-db-sync/index.html#12/
+        "mainnet" = "https://update-cardano-mainnet.iohk.io/cardano-db-sync/12/db-sync-snapshot-schema-12-block-6878999-x86_64.tgz";
+      };
+      snapShotSha = {
+        "testnet" = "4c0bf34537ff4c703ba41e7613291dea240623d81ac9f55973042a2e2f7dc95c";
+        "mainnet" = "f022340ce3e7fd0ac9492d00e9e996dd6158c68b72fc4af67f266ca0e79d2e55";
+      };
     in
       {
         "node-${envName}-entrypoint" = writeShellApplication {
@@ -74,6 +84,8 @@ in
                   inherit socketPath envFlag;
                   schemaDir = inputs.cardano-db-sync + "/schema";
                   stateDir = dbSyncStateDir;
+                  snapShotUrl = snapShotUrl.${envName};
+                  snapShotSha = snapShotSha.${envName};
                   configFile = builtins.toFile "db-sync-config.json" (
                     builtins.toJSON (
                       cardanoLib.environments.${envName}.explorerConfig
@@ -85,10 +97,12 @@ in
                 runtimeInputs = [
                   packages.cardano-db-sync
                   packages.cardano-cli
-                  nixpkgs.postgresql_12
                   nixpkgs.coreutils
+                  nixpkgs.curl
                   nixpkgs.dig
+                  nixpkgs.gnutar
                   nixpkgs.jq
+                  nixpkgs.postgresql_12
                 ];
               }
             )

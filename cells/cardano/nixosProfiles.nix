f5d94b0e21065177d8953dac48a28ec57cfc3cdf
Shay Bergmann <shay.bergmann@iohk.io>
2022-03-02 20:13:50 +0000

    Add missing parentheses

--- cells/cardano/nixosProfiles.nix
+++ cells/cardano/nixosProfiles.nix
@@ -70,6 +70,7 @@ in rec {
           / 3
         );
     };
+  };
   client = namespace: { bittelib
   , ...
   }:
--- cells/patroni/nixosProfiles.nix
+++ cells/patroni/nixosProfiles.nix
@@ -17,6 +17,7 @@
         };
       };
     };
+  };
   client = namespace: { bittelib
   , ...
   }:

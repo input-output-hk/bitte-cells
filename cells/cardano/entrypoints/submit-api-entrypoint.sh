29fa6461dc716175598c3801aa8401ccd6521c56
Shay Bergmann <shay.bergmann@iohk.io>
2022-03-10 18:41:36 +0000

    cardano: fix submit-api invocation error

--- cells/cardano/entrypoints/submit-api-entrypoint.sh
+++ cells/cardano/entrypoints/submit-api-entrypoint.sh
@@ -6,12 +6,15 @@ trap 'echo "$(date -u +"%b %d, %y %H:%M:%S +0000"): Caught SIGINT -- exiting" &&
 [ -z "${envFlag:-}" ] && echo "envFlag env var must be set -- aborting" && exit 1
 [ -z "${configFile:-}" ] && echo "configFile env var must be set -- aborting" && exit 1
 
+IFS=" " read -r -a envFlag <<< "${envFlag}"
+
 cmd=(
   cardano-submit-api
   --socket-path "${socketPath}"
   --port 8090
   --listen-address "0.0.0.0"
   --config "${configFile}"
-  "${envFlag}"
+  "${envFlag[@]}"
 )
+
 exec "${cmd[@]}"

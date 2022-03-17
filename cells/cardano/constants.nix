3bd42921262484ebea2f2b50236ba2ad03e98aaf
Samuel Leathers <samuel.leathers@iohk.io>
2022-03-17 12:38:33 -0400

    update db-sync snapshots

--- cells/cardano/constants.nix
+++ cells/cardano/constants.nix
@@ -15,13 +15,13 @@ in {
     dbSync = {
       snapShotUrl = {
         # https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/index.html#12/
-        "testnet" = "https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/12/db-sync-snapshot-schema-12-block-3336499-x86_64.tgz";
+        "testnet" = "https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/12/db-sync-snapshot-schema-12-block-3374999-x86_64.tgz";
         # https://update-cardano-mainnet.iohk.io/cardano-db-sync/index.html#12/
-        "mainnet" = "https://update-cardano-mainnet.iohk.io/cardano-db-sync/12/db-sync-snapshot-schema-12-block-6878999-x86_64.tgz";
+        "mainnet" = "https://update-cardano-mainnet.iohk.io/cardano-db-sync/12/db-sync-snapshot-schema-12-block-6965999-x86_64.tgz";
       };
       snapShotSha = {
-        "testnet" = "4c0bf34537ff4c703ba41e7613291dea240623d81ac9f55973042a2e2f7dc95c";
-        "mainnet" = "f022340ce3e7fd0ac9492d00e9e996dd6158c68b72fc4af67f266ca0e79d2e55";
+        "testnet" = "dd3f0fbbe205aa5e9d768de673dcdfd4f01bfedb6d8e10dc8ecc691055739846";
+        "mainnet" = "3096a45f27791e661823bb521b25ada30c57df84e835fbbae729ed25f7eec63e";
       };
     };
   };

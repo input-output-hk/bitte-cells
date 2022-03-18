0576ba6d822ffefa1ae7614c58febcff7f31cffd
David Arnold <david.arnold@iohk.io>
2022-03-17 22:43:24 -0500

    fix(cardano): add iana-etc for haskell internet

--- cells/cardano/oci-images.nix
+++ cells/cardano/oci-images.nix
@@ -12,7 +12,7 @@ in (
         name = "docker.infra.aws.iohkdev.io/cardano-node-${envName}";
         tag = inputs.self.rev;
         maxLayers = 15;
-        contents = [nixpkgs.bashInteractive];
+        contents = [nixpkgs.bashInteractive nixpkgs.iana-etc];
         config.Cmd = [
           "${entrypoints."node-${envName}-entrypoint"}/bin/cardano-node-${envName}-entrypoint"
         ];
@@ -21,7 +21,7 @@ in (
         name = "docker.infra.aws.iohkdev.io/cardano-submit-api-${envName}";
         tag = inputs.self.rev;
         maxLayers = 15;
-        contents = [nixpkgs.bashInteractive];
+        contents = [nixpkgs.bashInteractive nixpkgs.iana-etc];
         config.Cmd = [
           "${entrypoints."submit-api-${envName}-entrypoint"}/bin/cardano-submit-api-${envName}-entrypoint"
         ];
@@ -30,7 +30,7 @@ in (
         name = "docker.infra.aws.iohkdev.io/cardano-db-sync-${envName}";
         tag = inputs.self.rev;
         maxLayers = 15;
-        contents = [nixpkgs.bashInteractive];
+        contents = [nixpkgs.bashInteractive nixpkgs.iana-etc];
         config.Cmd = [
           "${entrypoints."db-sync-${envName}-entrypoint"}/bin/cardano-db-sync-${envName}-entrypoint"
         ];
@@ -39,7 +39,7 @@ in (
         name = "docker.infra.aws.iohkdev.io/cardano-wallet-${envName}";
         tag = inputs.self.rev;
         maxLayers = 15;
-        contents = [nixpkgs.bashInteractive];
+        contents = [nixpkgs.bashInteractive nixpkgs.iana-etc];
         config.Cmd = [
           "${entrypoints."wallet-${envName}-entrypoint"}/bin/cardano-wallet-${envName}-entrypoint"
         ];
@@ -52,7 +52,7 @@ in (
         name = "docker.infra.aws.iohkdev.io/cardano-wallet-init";
         tag = inputs.self.rev;
         maxLayers = 15;
-        contents = [nixpkgs.bashInteractive];
+        contents = [nixpkgs.bashInteractive nixpkgs.iana-etc];
         config.Cmd = [
           "${entrypoints.wallet-init-entrypoint}/bin/cardano-wallet-init-entrypoint"
         ];

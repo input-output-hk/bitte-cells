ac18d5beb962749769447238c908fcfef4c4cefb
John Lotoski <john.lotoski@iohk.io>
2022-05-10 22:21:38 -0500

    imp: updates db-sync testnet artifact

--- cells/cardano/constants.nix
+++ cells/cardano/constants.nix
@@ -26,12 +26,12 @@ in
       dbSync = {
         snapShotUrl = {
           # https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/index.html#12/
-          "testnet" = "https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/12/db-sync-snapshot-schema-12-block-3374999-x86_64.tgz";
+          "testnet" = "https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/12/db-sync-snapshot-schema-12-block-3514204-x86_64.tgz";
           # https://update-cardano-mainnet.iohk.io/cardano-db-sync/index.html#12/
           "mainnet" = "https://update-cardano-mainnet.iohk.io/cardano-db-sync/12/db-sync-snapshot-schema-12-block-6965999-x86_64.tgz";
         };
         snapShotSha = {
-          "testnet" = "dd3f0fbbe205aa5e9d768de673dcdfd4f01bfedb6d8e10dc8ecc691055739846";
+          "testnet" = "d0432e6a2dec2b6fce019ad1a31f124cd816df9c4dccd2317f5e71b05d794a78";
           "mainnet" = "3096a45f27791e661823bb521b25ada30c57df84e835fbbae729ed25f7eec63e";
         };
       };

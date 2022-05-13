e66642037cf0ca6fed134c2084e1f2f0ee575f2c
David Arnold <david.arnold@iohk.io>
2022-05-13 16:53:04 -0500

    imp: adding some debugging tools to the debug image

--- cells/_utils/library.nix
+++ cells/_utils/library.nix
@@ -3,16 +3,34 @@
   cell,
 }: let
   inherit (inputs) nixpkgs;
+  n2c = inputs.n2c.packages.nix2container;
 in {
   mkDebugOCI = entrypoint: oci: let
     iog-debug-banner = nixpkgs.runCommandNoCC "iog-debug-banner" {} ''
       ${nixpkgs.figlet}/bin/figlet -f banner "IOG Debug" > $out
     '';
+    debug-tools = [
+      nixpkgs.bashInteractive
+      nixpkgs.coreutils
+      nixpkgs.inetutils
+      nixpkgs.findutils
+      nixpkgs.dnsutils
+      nixpkgs.ripgrep
+      nixpkgs.strace
+      nixpkgs.curlie
+      nixpkgs.gawk
+      nixpkgs.jq
+      nixpkgs.fd
+    ];
+    debug-tools-layer = n2c.buildLayer {deps = debug-tools;};
     debug-bin = nixpkgs.writeShellApplication {
       name = "debug";
-      runtimeInputs = entrypoint.runtimeInputs ++ entrypoint.debugInputs;
+      runtimeInputs =
+        entrypoint.runtimeInputs
+        ++ entrypoint.debugInputs
+        ++ debug-tools;
       text = ''
-        ${nixpkgs.coreutils}/bin/cat ${iog-debug-banner}
+        cat ${iog-debug-banner}
         echo
         echo "=========================================================="
         echo "This debug shell contains the runtime environment and "

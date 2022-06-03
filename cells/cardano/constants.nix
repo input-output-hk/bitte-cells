8591908e01cec3f2c0a27281694265d7da98bd35
Omega Dilebo <omega.meseret@iohk.io>
2022-06-03 14:02:59 +0000

    imp: update db-sync artifact

--- cells/cardano/constants.nix
+++ cells/cardano/constants.nix
@@ -26,12 +26,12 @@ in
       dbSync = {
         snapShotUrl = {
           # https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/index.html#12/
-          "testnet" = "https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/12/db-sync-snapshot-schema-12-block-3514204-x86_64.tgz";
+          "testnet" = "https://updates-cardano-testnet.s3.amazonaws.com/cardano-db-sync/12/db-sync-snapshot-schema-12-block-3590461-x86_64.tgz";
           # https://update-cardano-mainnet.iohk.io/cardano-db-sync/index.html#12/
           "mainnet" = "https://update-cardano-mainnet.iohk.io/cardano-db-sync/12/db-sync-snapshot-schema-12-block-6965999-x86_64.tgz";
         };
         snapShotSha = {
-          "testnet" = "d0432e6a2dec2b6fce019ad1a31f124cd816df9c4dccd2317f5e71b05d794a78";
+          "testnet" = "922bb683158f36380d9e54399a27ac3bb5d760bdd4bc55f00f35ea40ae6d0f5f";
           "mainnet" = "3096a45f27791e661823bb521b25ada30c57df84e835fbbae729ed25f7eec63e";
         };
       };

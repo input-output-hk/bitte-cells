7470f700fee953127af4923cad8b6ff24236ccb1
Samuel Leathers <samuel.leathers@iohk.io>
2022-06-03 15:18:24 -0400

    remove cardano-cell

--- cells/cardano/healthChecks/wallet-network-sync-check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-STATUS="$(curl -sf "http://localhost:8090/v2/network/information" || :)"
-jq <<<"$STATUS" || :
-jq -e '.sync_progress.status == "ready"' <<<"$STATUS" || exit 1
--- flake.nix
+++ flake.nix
@@ -5,11 +5,6 @@
     n2c.url = "github:nlewo/nix2container";
     data-merge.url = "github:divnix/data-merge";
     cicero.url = "github:input-output-hk/cicero";
-    # Cardano Stack Inputs
-    cardano-iohk-nix.url = "github:input-output-hk/iohk-nix";
-    cardano-node.url = "github:input-output-hk/cardano-node/flake-improvements";
-    cardano-db-sync.url = "github:input-output-hk/cardano-db-sync/12.0.1-flake-improvements";
-    cardano-wallet.url = "github:input-output-hk/cardano-wallet";
   };
 
   outputs = {
@@ -27,7 +22,6 @@
         "x86_64-linux"
       ];
       cellsFrom = ./cells;
-      # debug = ["cells" "cardano" "healthChecks"];
       organelles = [
         (std.runnables "healthChecks")
         (std.runnables "entrypoints")

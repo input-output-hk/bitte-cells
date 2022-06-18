471787c664003839c25fe5c887b31e3810c5d299
Timothy DeHerrera <tim.deherrera@iohk.io>
2022-06-17 20:26:04 -0700

    utils: cut the fat out of Debug OCI images
    Use only statically built packages to keep the closure size under
    control, removing any tools that dont have one. Also use busybox, which
    is tailor-made for lean environments.

--- cells/_utils/library.nix
+++ cells/_utils/library.nix
@@ -5,49 +5,43 @@
   inherit (inputs) nixpkgs;
   n2c = inputs.n2c.packages.nix2container;
 in {
-  mkDebugOCI = entrypoint: oci: let
-    iog-debug-banner = nixpkgs.runCommandNoCC "iog-debug-banner" {} ''
-      ${nixpkgs.figlet}/bin/figlet -f banner "IOG Debug" > $out
-    '';
-    debug-tools = [
-      nixpkgs.bashInteractive
-      nixpkgs.coreutils
-      nixpkgs.inetutils
-      nixpkgs.findutils
-      nixpkgs.dnsutils
-      nixpkgs.ripgrep
-      nixpkgs.strace
-      nixpkgs.curl
-      nixpkgs.gawk
-      nixpkgs.jq
-      nixpkgs.fd
-    ];
-    debug-tools-layer = n2c.buildLayer {deps = debug-tools;};
-    debug-bin = nixpkgs.writeShellApplication {
-      name = "debug";
-      runtimeInputs =
-        entrypoint.runtimeInputs
-        or []
-        ++ entrypoint.debugInputs or []
-        ++ debug-tools;
-      text = ''
-        # shellcheck source=/dev/null
-        # source ${nixpkgs.cacert}/nix-support/setup-hook
-        cat ${iog-debug-banner}
-        echo
-        echo "=========================================================="
-        echo "This debug shell contains the runtime environment and "
-        echo "debug dependencies of the entrypoint."
-        echo "To inspect the entrypoint(s) run:"
-        echo "cat ${entrypoint}/bin/*"
-        echo "=========================================================="
-        echo
-        exec bash "$@"
+  mkDebugOCI = with nixpkgs.pkgsStatic;
+    entrypoint: oci: let
+      iog-debug-banner = runCommandNoCC "iog-debug-banner" {} ''
+        ${figlet}/bin/figlet -f banner "IOG Debug" > $out
       '';
-    };
-  in
-    oci
-    // {
-      contents = (oci.contents or []) ++ [debug-bin];
-    };
+      debug-tools = [
+        bashInteractive.out
+        busybox
+        curl.bin
+        jq.bin
+      ];
+      debug-bin = writeShellApplication {
+        name = "debug";
+        runtimeInputs =
+          entrypoint.runtimeInputs
+          or []
+          ++ entrypoint.debugInputs or []
+          ++ debug-tools;
+        text = with nixpkgs.pkgsStatic; ''
+          # shellcheck source=/dev/null
+          # source ''${cacert}/nix-support/setup-hook
+
+          cat ${iog-debug-banner}
+          echo
+          echo "=========================================================="
+          echo "This debug shell contains the runtime environment and "
+          echo "debug dependencies of the entrypoint."
+          echo "To inspect the entrypoint(s) run:"
+          echo "cat ${entrypoint}/bin/*"
+          echo "=========================================================="
+          echo
+          exec bash "$@"
+        '';
+      };
+    in
+      oci
+      // {
+        contents = (oci.contents or []) ++ [debug-bin];
+      };
 }

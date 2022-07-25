15a751152e8dcab612bfbb6baa92e810980bc4a9
John Lotoski <john.lotoski@iohk.io>
2022-07-25 12:57:12 -0500

    fix: patroni metrics temp. w/ localhost scrape
    * Metrics endpoint scrape from host was broken on switch to bridge mode
      networking.
    * By default Nomad =< 1.3.2 disallows bridged hairpin packets.
    * Temporarily work around this issue by scraping localhost until fixed
      upstream.

--- cells/patroni/nomadJob/default.nix
+++ cells/patroni/nomadJob/default.nix
@@ -91,12 +91,19 @@ in
           merge
           (cells.vector.nomadTask.default {
             inherit namespace;
-            # Patroni requires the rest interface to be publicly
-            # bound, accessible over the network by other cluster
-            # members and the metrics endpoint is served from the
-            # same binding.  Therefore, we need to scrape the host
-            # IP:PORT for metrics.
-            endpoints = ["https://$NOMAD_ADDR_patroni/metrics"];
+            # TODO: Once network bridge mode hairpinning is fixed
+            # switch to using the host IP to improve endpoint metrics
+            # reporting which now will report only 127.0.0.1 for each
+            # patroni member, with the distinguishing metric being
+            # nomad_alloc_id.
+            #
+            # Refs:
+            #   https://github.com/hashicorp/nomad/issues/13352
+            #   https://github.com/hashicorp/nomad/pull/13834
+            #
+            # Switch to:
+            # endpoints = ["https://$NOMAD_ADDR_patroni/metrics"];
+            endpoints = ["https://127.0.0.1:8008/metrics"];
 
             extra = {
               # Until we implement app based mTLS, or alternatively
